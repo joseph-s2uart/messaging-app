2ab33d59ba22fc5308f2dfba456ae0bec4390dd6
Joseph Stuart <68748410+joseph-s2uart@users.noreply.github.com>
2021-03-31 01:15:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,9 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQVRCq5hglL7TOZV7OPtJvlu5Gjce09Yc",
-  authDomain: "messaging-app-5b75d.firebaseapp.com",
-  databaseURL: "https://messaging-app-5b75d-default-rtdb.firebaseio.com",
-  projectId: "messaging-app-5b75d",
-  storageBucket: "messaging-app-5b75d.appspot.com",
-  messagingSenderId: "423685813986",
-  appId: "1:423685813986:web:15d5a0534fa895c44150bb",
-  measurementId: "G-FJKV7STT6K"
+ 
+  
+  // paste firebase config code from Firebase
 };
 
 // eslint-disable-next-line no-unused-vars
